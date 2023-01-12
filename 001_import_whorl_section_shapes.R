8428337bf86dc6768fbec2df0822107d12981280
Sam <ginotsam@gmail.com>
2023-01-12 17:13:49 +0100

    Original commit: creation of an R project and of the first R script (import whorl sections).

---
+++ 001_import_whorl_section_shapes.R
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Updated and cleaned up script for importing ammonoid whorl section data from
+# JPG images. These are black shapes on white backgrounds, extracted from the
+# Primqry litterqture
+
+# This specific script uses package Momocs to import those shapes as matrices of
+# X/Y coordinates.
+
+# Required packages
+
+library(Momocs)
+#-------------------------------------------------------------------------------
