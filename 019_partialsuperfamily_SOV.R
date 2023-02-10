dce2a205a9b3bf4717ab85570ed70f59e9a8229e
sginot <ginotsam@gmail.com>
2023-02-10 21:29:09 +0100

    Commit 23: Added the Foote partial disparity computation and plotting again, with updated figures.

--- 019_partialsuperfamily_SOV.R
+++ 019_partialsuperfamily_SOV.R
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
-# Calculate SOV per time bin and per superfamily (ie "partial disparity"), and
-# plot as barplots. Note thqt "partial" Sum of variances is perhaps not the best
-# way to describe it. Note this will apply the script for conch ratio data only.
+# Calculate SOV per time bin and per superfamily and plot as barplots. 
+# Also calculate partial disparity in the fashion used by Foote 1993.
+# Note this will apply the script for conch ratio data only.
 
 # Required packages:
 library(scales)
@@ -35,11 +35,20 @@ cols <- c("#F7941D",
           "#8CC63F", 
           "#ED247E")
 #-------------------------------------------------------------------------------
+# Load files with ages for time bins
 
 intervals_ages <- read.csv("intervals_dates.csv",
                            h = T,
                            sep= ",",
                            dec = ".")
+
+biozones_ages <- read.csv("biozones_dates.csv",
+                          h = T,
+                          sep= ",",
+                          dec = ".")
+
+#-------------------------------------------------------------------------------
+# Compute and plot "Partial SOV" at interval level 
 
 PSOV <- list()
 
@@ -103,13 +112,7 @@ legend("topleft",
 dev.off()
 
 #-------------------------------------------------------------------------------
-# Do the same thing at the biozones level 
-
-
-biozones_ages <- read.csv("biozones_dates.csv",
-                           h = T,
-                           sep= ",",
-                           dec = ".")
+# Compute and plot "Partial SOV" at biozone level 
 
 PSOV <- list()
 
@@ -164,6 +167,163 @@ pdf(paste(output_folder,
 par(mar = c(22, 4, 1, 1))
 barplot(mat, 
         ylab = "SOV",
+        width = rev(biozones_ages[,3]),
+        space = 0,
+        names.arg = rev(biozones_ages[,1]),
+        col = cols,
+        las = 2)
+
+legend("topleft", 
+       lty = 1, 
+       lwd = 10, 
+       cex = 1.4, 
+       col = cols, 
+       legend = c("Agoniatitoidea", 
+                  "Anarcestoidea", 
+                  "Gephuroceratoidea",
+                  "Mimagoniatitoidea", 
+                  "Mimosphinctoidea",
+                  "Pharciceratoidea",
+                  "Tornoceratoidea"))
+
+dev.off()
+
+#-------------------------------------------------------------------------------
+# Foote (1993) similar figure of partial disparities
+# First at interval level
+
+MD <- rep(NA, 7)
+
+PD <- list()
+
+for (i in 1:7){
+  
+  tryCatch({
+    
+    submorphospace <- pca$x[which(interval_factor == Interval[i]),]
+    
+    N <- nrow(submorphospace)
+    
+    centroid <- apply(submorphospace, 
+                      2, 
+                      mean)
+    
+    mat <- rbind(centroid, 
+                 submorphospace)
+    
+    distcent <- dist(mat)[1:N]
+    
+    parts <- rep(NA, 7)
+    
+    for (j in 1:7) {
+      
+      super <- list.superfamily.interval[[i]]
+      
+      parts[j] <- sum(distcent[which(super == levels(super)[j])]^2)/(N-1)
+      
+    }
+    
+    PD[[i]] <- parts
+    
+    MD[i] <- sum(distcent^2)/(N-1)}, 
+    
+    error = function(a){return(NA)})
+  
+}
+
+mat <- matrix(NA, 
+              ncol = 7,
+              nrow = 7)
+
+for(i in 1:7) {mat[, i] <- PD[[i]]}
+
+pdf(paste(output_folder,
+          "Partial_disparity_Foote_style_interval_conch.pdf",
+          sep = ""))
+
+barplot(mat, 
+        ylab = "Sum of squared Euclidean distance from centroid",
+        width = rev(intervals_ages[,3]),
+        space = 0,
+        names.arg = rev(intervals_ages[,1]),
+        col = cols,
+        las = 2)
+
+legend("topleft", 
+       lty = 1, 
+       lwd = 10, 
+       cex = 0.7, 
+       col = cols, 
+       legend = c("Agoniatitoidea", 
+                  "Anarcestoidea", 
+                  "Gephuroceratoidea",
+                  "Mimagoniatitoidea", 
+                  "Mimosphinctoidea",
+                  "Pharciceratoidea",
+                  "Tornoceratoidea"))
+
+dev.off()
+
+#-------------------------------------------------------------------------------
+# Foote (1993) similar figure of partial disparities
+# Then at biozone level
+
+MD <- rep(NA, 30)
+
+PD <- list()
+
+for (i in 1:30) {
+  
+    submorphospace <- pca$x[which(biozone_factor == Biozones[i]),]
+    
+    N <- nrow(submorphospace)
+    
+    centroid <- apply(submorphospace, 
+                      2, 
+                      mean)
+    
+    mat <- rbind(centroid, 
+                 submorphospace)
+    
+    distcent <- dist(mat)[1:N]
+    
+    parts <- rep(NA, 7)
+    
+    for (j in 1:7) {
+      
+      super <- list.superfamily.biozones[[i]]
+      
+      tryCatch({
+        
+      parts[j] <- sum(distcent[which(super == levels(super)[j])]^2)/(N-1)}, 
+      
+      error = function(a) {return(NA)})
+      
+    }
+    
+    PD[[i]] <- parts
+    
+    MD[i] <- sum(distcent^2)/(N-1)
+  
+}
+
+mat <- matrix(NA, 
+              ncol = 30, 
+              nrow = 7)
+
+for (i in 1:30) {mat[,i] <- PD[[i]]}
+
+mat <- mat[,ord]
+
+pdf(paste(output_folder,
+          "Partial_disparity_Foote_style_biozones_conch.pdf",
+          sep = ""),
+    width = 11,
+    height = 14)
+
+par(mar = c(22, 4, 1, 1))
+barplot(mat, 
+        ylab = "Sum of squared Euclidean distance from centroid",
         width = rev(biozones_ages[,3]),
         space = 0,
         names.arg = rev(biozones_ages[,1]),
--- 020_partial_SOV_WHORL.R
+++ 020_partial_SOV_WHORL.R
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
-# Calculate SOV per time bin and per superfamily (ie "partial disparity"), and
-# plot as barplots. Note thqt "partial" Sum of variances is perhaps not the best
-# way to describe it. This script applies to whorl sections data.
+# Calculate SOV per time bin and per superfamily and plot as barplots. 
+# Calculate Foote (1993) style partial disparity and plot.
+# This script applies to whorl sections data.
 
 # Required packages:
 library(scales)
@@ -35,11 +35,19 @@ cols <- c("#F7941D",
           "#8CC63F", 
           "#ED247E")
 #-------------------------------------------------------------------------------
+# Load files with ages and names for time bins
 
 intervals_ages <- read.csv("intervals_dates.csv",
                            h = T,
                            sep= ",",
                            dec = ".")
+
+biozones_ages <- read.csv("biozones_dates.csv",
+                          h = T,
+                          sep= ",",
+                          dec = ".")
+
+#-------------------------------------------------------------------------------
 
 PSOV <- list()
 
@@ -105,12 +113,6 @@ dev.off()
 
 #-------------------------------------------------------------------------------
 # Do the same thing at the biozones level 
-
-
-biozones_ages <- read.csv("biozones_dates.csv",
-                         h = T,
-                         sep= ",",
-                         dec = ".")
 
 ls.sup.ord <- list.superfamily.biozones[ord]
 ls.taxa.ord <- list.taxa.biozones[ord]
@@ -187,3 +189,164 @@ legend("topleft",
                   "Tornoceratoidea"))
 
 dev.off()
+
+
+#-------------------------------------------------------------------------------
+# Foote (1993) similar figure of partial disparities
+# First at interval level
+
+MD <- rep(NA, 7)
+
+PD <- list()
+
+for (i in 1:7){
+  
+  tryCatch({
+    
+    submorphospace <- pca$x[match(list.taxa.intervals[[i]], 
+                                  levels(Ap.Image)), ]
+    
+    N <- nrow(submorphospace)
+    
+    centroid <- apply(submorphospace, 
+                      2, 
+                      mean)
+    
+    mat <- rbind(centroid, 
+                 submorphospace)
+    
+    distcent <- dist(mat)[1:N]
+    
+    parts <- rep(NA, 7)
+    
+    for (j in 1:7) {
+      
+      super <- list.superfamily.interval[[i]]
+      
+      parts[j] <- sum(distcent[which(super == levels(super)[j])]^2)/(N-1)
+      
+    }
+    
+    PD[[i]] <- parts
+    
+    MD[i] <- sum(distcent^2)/(N-1)}, 
+    
+    error = function(a){return(NA)})
+  
+}
+
+mat <- matrix(NA, 
+              ncol = 7,
+              nrow = 7)
+
+for(i in 1:7) {mat[, i] <- PD[[i]]}
+
+pdf(paste(output_folder,
+          "Partial_disparity_Foote_style_interval_whorl.pdf",
+          sep = ""))
+
+barplot(mat, 
+        ylab = "Sum of squared Euclidean distance from centroid",
+        width = rev(intervals_ages[,3]),
+        space = 0,
+        names.arg = rev(intervals_ages[,1]),
+        col = cols,
+        las = 2)
+
+legend("topleft", 
+       lty = 1, 
+       lwd = 10, 
+       cex = 0.7, 
+       col = cols, 
+       legend = c("Agoniatitoidea", 
+                  "Anarcestoidea", 
+                  "Gephuroceratoidea",
+                  "Mimagoniatitoidea", 
+                  "Mimosphinctoidea",
+                  "Pharciceratoidea",
+                  "Tornoceratoidea"))
+
+dev.off()
+
+#-------------------------------------------------------------------------------
+# Foote (1993) similar figure of partial disparities
+# Then at biozone level
+
+ls.sup.ord <- list.superfamily.biozones[ord]
+ls.taxa.ord <- list.taxa.biozones[ord]
+
+MD <- rep(NA, 30)
+
+PD <- list()
+
+for (i in 1:30) {
+  
+  submorphospace <- pca$x[match(ls.taxa.ord[[i]], 
+                                levels(Ap.Image)), ]  
+  N <- length(ls.taxa.ord[[i]])
+  
+  centroid <- apply(matrix(submorphospace, ncol = dim(pca$x)[2]), 
+                    2, 
+                    mean)
+  
+  mat <- rbind(centroid, 
+               submorphospace)
+  
+  distcent <- dist(mat)[1:N]
+  
+  parts <- rep(NA, 7)
+  
+  for (j in 1:7) {
+    
+    super <- ls.sup.ord[[i]]
+    
+    tryCatch({
+      
+      parts[j] <- sum(distcent[which(super == levels(super)[j])]^2)/(N-1)}, 
+      
+      error = function(a) {return(NA)})
+    
+  }
+  
+  PD[[i]] <- parts
+  
+  MD[i] <- sum(distcent^2)/(N-1)
+  
+}
+
+mat <- matrix(NA, 
+              ncol = 30, 
+              nrow = 7)
+
+for (i in 1:30) {mat[,i] <- PD[[i]]}
+
+pdf(paste(output_folder,
+          "Partial_disparity_Foote_style_biozones_whorl.pdf",
+          sep = ""),
+    width = 11,
+    height = 14)
+
+par(mar = c(22, 4, 1, 1))
+barplot(mat, 
+        ylab = "Sum of squared Euclidean distance from centroid",
+        width = rev(biozones_ages[,3]),
+        space = 0,
+        names.arg = rev(biozones_ages[,1]),
+        col = cols,
+        las = 2)
+
+legend("topleft", 
+       lty = 1, 
+       lwd = 10, 
+       cex = 1.4, 
+       col = cols, 
+       legend = c("Agoniatitoidea", 
+                  "Anarcestoidea", 
+                  "Gephuroceratoidea",
+                  "Mimagoniatitoidea", 
+                  "Mimosphinctoidea",
+                  "Pharciceratoidea",
+                  "Tornoceratoidea"))
+
+dev.off()
+
